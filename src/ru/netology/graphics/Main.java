c8e7bec1e933ae3e8c781a911771504bcfd58821
Vostrikov Vladislav <vvvostrikov11@gmail.com>
2022-06-16 17:38:46 +0300

    first

---
+++ src/ru/netology/graphics/Main.java
@@ -0,0 +1,21 @@
+package ru.netology.graphics;
+
+import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.server.GServer;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
+
+        GServer server = new GServer(converter); // Создаём объект сервера
+        server.start(); // Запускаем
+
+        // Или то же, но с выводом на экран:
+        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+        //String imgTxt = converter.convert(url);
+        //System.out.println(imgTxt);
+    }
+}
